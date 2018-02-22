d882f0899def9c936236763d4d37d5a394a36bb1
Kyle Coberly <kyle.coberly@gmail.com>
2018-02-22 15:42:50 -0700

    Twitter working

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+require("dotenv").load();
 const express = require("express");
 const app = express();
 
+const Twitter = require("twitter");
+const twitter = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
 app.get("/", (request, response) => {
-    response.send("Hey");
+    var params = {screen_name: "nodejs"};
+    twitter.get("statuses/user_timeline", params, function(error, tweets, twitterResponse) {
+        if (!error) {
+            console.log(tweets);
+            response.send({tweets});
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000);
